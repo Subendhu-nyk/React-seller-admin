6a474bcecdbe3903287bc5212b8c0660379c9d2f
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-10-28 14:37:41 +0530

    improved

--- src/Components/AddProducts/ProductList.js
+++ src/Components/AddProducts/ProductList.js
@@ -6,9 +6,13 @@ const ProductList=(props)=>{
 return(
     <Card className={classes.products}>
         <ul>
+            
             {
-                props.products.map((product)=>(
+                props.products.map((product)=>( 
+                    <div>
+                    <h3>{product.category}</h3>                  
                     <li key={product.id}>Product Id -- {product.product_id} || Product Price-- {product.price} || Product Name -- {product.name} || Product Category -- {product.category}</li>
+                    </div> 
                 ))
             }
         </ul>
